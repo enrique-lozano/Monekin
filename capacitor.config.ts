bcefa9901ec6088cf8b4a1f76ce7a1fa9eb4d231
enriqueloz88 <lozano.elc@gmail.com>
2022-12-06 19:45:40 +0100

    feat: Begin the Ionic project

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.monekin.app',
+  appName: 'Monekin',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
