6e7afe1991ece8c2a0023a027e0fcdc37ac05d03
enriqueloz88 <lozano.elc@gmail.com>
2022-12-10 13:45:45 +0100

    feat(literals): Add missing translations

--- src/app/pages/tabs/tabs.module.ts
+++ src/app/pages/tabs/tabs.module.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { TranslocoModule } from '@ngneat/transloco';
 
 import { TabsPage } from './tabs.page';
 
 @NgModule({
-  imports: [IonicModule, CommonModule, FormsModule],
+  imports: [IonicModule, CommonModule, FormsModule, TranslocoModule],
   declarations: [TabsPage],
   exports: [TabsPage],
 })
