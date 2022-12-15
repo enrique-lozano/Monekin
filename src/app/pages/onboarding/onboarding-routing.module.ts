1c48dbbb060a58b836327b3a93232def39c24186
enriqueloz88 <lozano.elc@gmail.com>
2022-12-15 20:02:03 +0100

    refactor: Rename the intro slides to `onboarding`

--- src/app/pages/onboarding/onboarding-routing.module.ts
+++ src/app/pages/onboarding/onboarding-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { IntroPage } from './intro.page';
+import { OnboardingPage } from './onboarding.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: IntroPage,
+    component: OnboardingPage,
   },
 ];
 
@@ -14,4 +14,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class IntroPageRoutingModule {}
+export class OnboardingPageRoutingModule {}
