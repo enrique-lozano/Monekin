cb3620ad257bb847e345fbf7bb1b0ca9ab150f99
enriqueloz88 <lozano.elc@gmail.com>
2022-12-20 14:02:33 +0100

    fix: Toast appearing ahead tabs and footer

--- src/app/services/ionic/toast.service.ts
+++ src/app/services/ionic/toast.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { ToastController } from '@ionic/angular';
 import { LangService } from '../translate/translate.service';
 
@@ -8,7 +9,8 @@ import { LangService } from '../translate/translate.service';
 export class ToastService {
   constructor(
     private toastController: ToastController,
-    private lang: LangService
+    private lang: LangService,
+    private router: Router
   ) {}
 
   /** Present a toast at the bottom of the page
@@ -28,6 +30,53 @@ export class ToastService {
       color: color == 'default' ? undefined : color,
     });
 
+    /** The main fab button near the tabs (only in mobile) */
+    const fabButton = document.getElementById(
+      'add-transaction-mobile-fab'
+    ) as HTMLElement;
+
+    if (fabButton) {
+      fabButton.style.transform = 'scale(0)';
+    }
+
+    /** The toast should be displayed above the bottomEl (if any) */
+    let bottomEl = document.querySelector('app-tabs.show-on-mobile > *');
+
+    if (
+      bottomEl &&
+      window.innerWidth <
+        Number(
+          getComputedStyle(document.documentElement)
+            .getPropertyValue('--breakpoint-sm')
+            .replace('px', '')
+            .trim()
+        )
+    ) {
+      toast.style.transform = `translateY(-${bottomEl.clientHeight}px)`;
+    } else {
+      bottomEl = document.querySelector('ion-footer');
+      if (bottomEl) {
+        toast.style.transform = `translateY(-${bottomEl.clientHeight}px)`;
+      }
+    }
+
     toast.present();
+
+    setTimeout(() => {
+      // With this timeout we are sure that the transition to another page is finished (if any). For example, when you navigate to another page and immediately open the toast
+      const subscr = this.router.events.subscribe((val) => {
+        if (val instanceof NavigationEnd) {
+          toast.dismiss();
+        }
+      });
+
+      toast.onDidDismiss().then(() => {
+        subscr.unsubscribe();
+      });
+    }, 1500);
+
+    toast.onDidDismiss().then(() => {
+      fabButton.style.transform = '';
+    });
   }
 }
