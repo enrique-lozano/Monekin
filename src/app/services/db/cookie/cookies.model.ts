ace3e2e9ab8738c3655c493e877b65be03e4f76f
enriqueloz88 <lozano.elc@gmail.com>
2022-12-21 23:08:23 +0100

    refactor: Add some comments

--- src/app/services/db/cookie/cookies.model.ts
+++ src/app/services/db/cookie/cookies.model.ts
@@ -1,7 +1,14 @@
 /** Some variables necessary for the functionality of the app. the user cannot modify this variables (unlike what happens with the Setting interface object) */
 export interface Cookies {
+  /** The current DB model version. Since Monekin `v3.0.0` this version is equivalent to the version of the app.
+   *
+   * In this way, even if there are no changes in the data model, this parameter will be updated with each Monekin update. */
   modelVersion: string;
+
+  /** Indicates if the user has seen the intro/onboarding */
   introSeen: boolean;
+
+  /** Date of the last exportation of the user */
   lastExportDate: Date;
 }
 
