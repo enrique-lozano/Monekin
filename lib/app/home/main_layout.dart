3a10c9feed98fa8e41aa1d9dc9ecc0f22a073de7
enriqueloz88 <lozano.elc@gmail.com>
2023-11-26 22:52:37 +0100

    fix(ux/ui): Add safe area on large screens

--- lib/app/home/main_layout.dart
+++ lib/app/home/main_layout.dart
@@ -33,94 +33,100 @@ class _MainLayoutPageState extends State<MainLayoutPage> {
                   (element) => element.destination.routeName == snapshot.data);
 
               return BreakpointContainer(
-                mdChild: Row(
-                  children: [
-                    NavigationRail(
-                        leading: const Column(
-                          children: [
-                            SizedBox(height: 16),
-                            NewTransactionButton(),
-                            SizedBox(height: 16),
-                          ],
-                        ),
-                        trailing: Expanded(
-                          child: Align(
-                            alignment: Alignment.bottomCenter,
-                            child: Padding(
-                              padding:
-                                  const EdgeInsets.only(bottom: 16, top: 16),
-                              child: StreamBuilder(
-                                  stream: UserSettingService.instance
-                                      .getSetting(SettingKey.avatar),
-                                  builder: (context, snapshot) {
-                                    return UserAvatar(avatar: snapshot.data);
-                                  }),
+                mdChild: SafeArea(
+                  child: Row(
+                    children: [
+                      NavigationRail(
+                          leading: const Column(
+                            children: [
+                              SizedBox(height: 16),
+                              NewTransactionButton(),
+                              SizedBox(height: 16),
+                            ],
+                          ),
+                          trailing: Expanded(
+                            child: Align(
+                              alignment: Alignment.bottomCenter,
+                              child: Padding(
+                                padding:
+                                    const EdgeInsets.only(bottom: 16, top: 16),
+                                child: StreamBuilder(
+                                    stream: UserSettingService.instance
+                                        .getSetting(SettingKey.avatar),
+                                    builder: (context, snapshot) {
+                                      return UserAvatar(avatar: snapshot.data);
+                                    }),
+                              ),
+                            ),
+                          ),
+                          labelType: NavigationRailLabelType.all,
+                          destinations: menuItems
+                              .map((e) => e.toNavigationRailDestinationWidget())
+                              .toList(),
+                          onDestinationSelected: (e) {
+                            if (e == selectedNavItemIndex) return;
+
+                            AutoRouter.of(context)
+                                .push(menuItems.elementAt(e).destination);
+                          },
+                          selectedIndex: selectedNavItemIndex < 0
+                              ? null
+                              : selectedNavItemIndex),
+                      const VerticalDivider(thickness: 2, width: 1),
+                      Expanded(
+                        child: Container(
+                          clipBehavior: Clip.hardEdge,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          margin: const EdgeInsets.all(16),
+                          child: Card(
+                            elevation: 4,
+                            clipBehavior: Clip.hardEdge,
+                            child: AutoRouter(
+                              key: mainLayoutRouterKey,
+                              navigatorObservers: () =>
+                                  [MainLayoutNavObserver()],
                             ),
                           ),
                         ),
-                        labelType: NavigationRailLabelType.all,
-                        destinations: menuItems
-                            .map((e) => e.toNavigationRailDestinationWidget())
-                            .toList(),
+                      )
+                    ],
+                  ),
+                ),
+                xlChild: SafeArea(
+                  child: Row(
+                    children: [
+                      HomeDrawer(
+                        drawerActions: menuItems,
                         onDestinationSelected: (e) {
                           if (e == selectedNavItemIndex) return;
 
                           AutoRouter.of(context)
                               .push(menuItems.elementAt(e).destination);
                         },
-                        selectedIndex: selectedNavItemIndex < 0
-                            ? null
-                            : selectedNavItemIndex),
-                    const VerticalDivider(thickness: 2, width: 1),
-                    Expanded(
-                      child: Container(
-                        clipBehavior: Clip.hardEdge,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        margin: const EdgeInsets.all(16),
-                        child: Card(
-                          elevation: 4,
+                        selectedIndex: selectedNavItemIndex,
+                      ),
+                      Expanded(
+                        child: Container(
                           clipBehavior: Clip.hardEdge,
-                          child: AutoRouter(
-                            key: mainLayoutRouterKey,
-                            navigatorObservers: () => [MainLayoutNavObserver()],
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          margin: const EdgeInsets.all(16),
+                          child: Card(
+                            elevation: 4,
+                            clipBehavior: Clip.hardEdge,
+                            child: AutoRouter(
+                              key: mainLayoutRouterKey,
+                              navigatorObservers: () =>
+                                  [MainLayoutNavObserver()],
+                            ),
                           ),
                         ),
-                      ),
-                    )
-                  ],
-                ),
-                xlChild: Row(
-                  children: [
-                    HomeDrawer(
-                      drawerActions: menuItems,
-                      onDestinationSelected: (e) {
-                        if (e == selectedNavItemIndex) return;
-
-                        AutoRouter.of(context)
-                            .push(menuItems.elementAt(e).destination);
-                      },
-                      selectedIndex: selectedNavItemIndex,
-                    ),
-                    Expanded(
-                      child: Container(
-                        clipBehavior: Clip.hardEdge,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        margin: const EdgeInsets.all(16),
-                        child: Card(
-                          elevation: 4,
-                          clipBehavior: Clip.hardEdge,
-                          child: AutoRouter(
-                            key: mainLayoutRouterKey,
-                            navigatorObservers: () => [MainLayoutNavObserver()],
-                          ),
-                        ),
-                      ),
-                    )
-                  ],
+                      )
+                    ],
+                  ),
                 ),
                 child: AutoRouter(
                   key: mainLayoutRouterKey,
