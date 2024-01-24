ace61939b25c95ae006675dfef592fb5cf276335
enriqueloz88 <lozano.elc@gmail.com>
2024-01-24 17:31:16 +0100

    refactor: Remove unnecesary comment

--- lib/core/database/services/currency/currency_service.dart
+++ lib/core/database/services/currency/currency_service.dart
@@ -8,8 +8,6 @@ class CurrencyService {
   final _currencyTableName = 'currencies';
   final _currencyNamesTableName = 'currencyNames';
 
-  // TODO: i18n
-
   String get _baseQuery =>
       'SELECT currency.code, currency.symbol, names.${LocaleSettings.currentLocale.languageCode} as name FROM $_currencyTableName as currency'
       ' JOIN $_currencyNamesTableName as names ON currency.code = names.currencyCode';
