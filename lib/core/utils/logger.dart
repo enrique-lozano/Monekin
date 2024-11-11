d9b156cf0ac705d67e1f93f5f3f8567c4ce7a74f
Vincentas Gedvilas <62765150+vincius0000@users.noreply.github.com>
2024-11-11 19:55:51 +0200

    Removed redundant method call

--- lib/core/utils/logger.dart
+++ lib/core/utils/logger.dart
@@ -3,7 +3,7 @@ import 'package:flutter/foundation.dart';
 class Logger {
   static void printDebug(Object message) {
     if (kDebugMode) {
-      print(message.toString());
+      print(message);
     }
   }
 }
