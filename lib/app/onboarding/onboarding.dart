5cf8706b80e990af9654455d4af3af52df3e0410
Enrique Lozano Cebriano <61509169+enrique-lozano@users.noreply.github.com>
2025-04-05 15:46:17 +0200

    feat(ux/ui): New onboarding steps design

--- lib/app/onboarding/onboarding.dart
+++ lib/app/onboarding/onboarding.dart
@@ -1,16 +1,20 @@
-import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:introduction_screen/introduction_screen.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:monekin/app/layout/tabs.dart';
 import 'package:monekin/core/database/services/app-data/app_data_service.dart';
 import 'package:monekin/core/database/services/currency/currency_service.dart';
 import 'package:monekin/core/database/services/user-setting/user_setting_service.dart';
+import 'package:monekin/core/extensions/color.extensions.dart';
+import 'package:monekin/core/models/currency/currency.dart';
+import 'package:monekin/core/presentation/animations/animated_expanded.dart';
+import 'package:monekin/core/presentation/app_colors.dart';
+import 'package:monekin/core/presentation/theme.dart';
 import 'package:monekin/core/presentation/widgets/currency_selector_modal.dart';
 import 'package:monekin/core/presentation/widgets/skeleton.dart';
 import 'package:monekin/core/routes/route_utils.dart';
 import 'package:monekin/i18n/generated/translations.g.dart';
 import 'package:monekin/main.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class OnboardingPage extends StatefulWidget {
   const OnboardingPage({super.key});
@@ -21,6 +25,7 @@ class OnboardingPage extends StatefulWidget {
 
 class _OnboardingPageState extends State<OnboardingPage> {
   int currentPage = 0;
+  PageController pageController = PageController();
 
   introFinished() {
     AppDataService.instance
@@ -36,6 +41,20 @@ class _OnboardingPageState extends State<OnboardingPage> {
     );
   }
 
+  _goToNextPage() {
+    pageController.nextPage(
+      duration: const Duration(milliseconds: 250),
+      curve: Curves.easeInOut,
+    );
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final t = Translations.of(context);
@@ -44,6 +63,10 @@ class _OnboardingPageState extends State<OnboardingPage> {
       {
         'header': t.intro.sl1_title,
         'description': t.intro.sl1_descr,
+        'extraWidget': Padding(
+          padding: const EdgeInsets.only(top: 16),
+          child: buildCurrencySelectorTile(context),
+        ),
         'image': 'assets/icons/app_onboarding/first.svg'
       },
       {
@@ -60,99 +83,7 @@ class _OnboardingPageState extends State<OnboardingPage> {
       },
     ];
 
-    List<PageViewModel> slides = items
-        .mapIndexed((index, item) => PageViewModel(
-            titleWidget: Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16),
-              child: Text(
-                item['header'],
-                style: Theme.of(context).textTheme.headlineLarge,
-                textAlign: TextAlign.center,
-              ),
-            ),
-            useRowInLandscape: true,
-            bodyWidget: Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16),
-              child: Column(children: [
-                Text(
-                  item['description'],
-                  style: const TextStyle(
-                      fontSize: 14.0, fontWeight: FontWeight.w300),
-                  textAlign: TextAlign.justify,
-                ),
-                if (item['description2'] != null) ...[
-                  const SizedBox(height: 10),
-                  Text(
-                    item['description2'],
-                    style: const TextStyle(
-                        fontSize: 14.0, fontWeight: FontWeight.w300),
-                    textAlign: TextAlign.justify,
-                  ),
-                ],
-                if (index == 0) ...[
-                  const SizedBox(height: 40),
-                  StreamBuilder(
-                      stream:
-                          CurrencyService.instance.getUserPreferredCurrency(),
-                      builder: (context, snapshot) {
-                        final userCurrency = snapshot.data;
-
-                        return ListTile(
-                          tileColor: Theme.of(context)
-                              .colorScheme
-                              .onSurface
-                              .withOpacity(0.04),
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10)),
-                          trailing: Icon(
-                            Icons.arrow_forward_ios_rounded,
-                            size: 14,
-                            color: Theme.of(context)
-                                .colorScheme
-                                .onSurface
-                                .withOpacity(0.45),
-                          ),
-                          leading: Container(
-                            clipBehavior: Clip.hardEdge,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(100),
-                            ),
-                            child: userCurrency != null
-                                ? userCurrency.displayFlagIcon(size: 42)
-                                : const Skeleton(height: 42, width: 42),
-                          ),
-                          title: Text(t.intro.select_your_currency),
-                          subtitle: userCurrency != null
-                              ? Text(userCurrency.name)
-                              : const Skeleton(height: 12, width: 50),
-                          onTap: () {
-                            if (userCurrency == null) return;
-
-                            showCurrencySelectorModal(
-                                context,
-                                CurrencySelectorModal(
-                                    preselectedCurrency: userCurrency,
-                                    onCurrencySelected: (newCurrency) {
-                                      UserSettingService.instance
-                                          .setItem(SettingKey.preferredCurrency,
-                                              newCurrency.code)
-                                          .then((value) => setState(() => {}));
-                                    }));
-                          },
-                        );
-                      }),
-                ],
-              ]),
-            ),
-            image: SvgPicture.asset(
-              item['image'],
-              fit: BoxFit.fitWidth,
-              width: 240.0,
-              alignment: Alignment.bottomCenter,
-            )))
-        .toList();
+    final isInLastPage = currentPage >= items.length - 1;
 
     return Scaffold(
         appBar: AppBar(
@@ -160,46 +91,180 @@ class _OnboardingPageState extends State<OnboardingPage> {
           elevation: 0,
           toolbarHeight: 0,
         ),
-        body: IntroductionScreen(
-          pages: slides,
-          showSkipButton: true,
-          initialPage: currentPage,
-          onChange: (value) {
-            setState(() {
-              currentPage = value;
-            });
+        floatingActionButton: FloatingActionButton.extended(
+          heroTag: null,
+          onPressed: () {
+            if (isInLastPage) {
+              introFinished();
+            } else {
+              _goToNextPage();
+            }
           },
-          skip: Text(
-            t.intro.skip,
-            style: const TextStyle(fontWeight: FontWeight.w300),
+          icon: const Icon(Icons.arrow_forward_ios_rounded),
+          tooltip: t.intro.next,
+          extendedPadding: const EdgeInsetsDirectional.only(start: 16, end: 16),
+          extendedIconLabelSpacing: isInLastPage ? 8 : 0,
+          label: AnimatedExpanded(
+            duration: const Duration(milliseconds: 250),
+            expand: isInLastPage,
+            axis: Axis.horizontal,
+            child: Text(t.ui_actions.continue_text),
           ),
-          next: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(t.intro.next),
-              const SizedBox(width: 4),
-              const Icon(Icons.arrow_forward)
-            ],
-          ),
-          done: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(t.ui_actions.continue_text),
-              const SizedBox(width: 4),
-              const Icon(Icons.check)
-            ],
-          ),
-          onDone: () => introFinished(),
-          onSkip: () => introFinished(),
-          dotsDecorator: DotsDecorator(
-            size: const Size.square(10.0),
-            activeSize: const Size(20.0, 10.0),
-            activeColor: Theme.of(context).colorScheme.primary,
-            color: Theme.of(context).colorScheme.primary.withOpacity(0.3),
-            spacing: const EdgeInsets.symmetric(horizontal: 3.0),
-            activeShape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(25.0)),
-          ),
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: LayoutBuilder(builder: (context, constraints) {
+                const expansionFactor = 4;
+                const spacing = 4;
+
+                return AnimatedSmoothIndicator(
+                  activeIndex: currentPage,
+                  count: items.length,
+                  effect: ExpandingDotsEffect(
+                    dotHeight: 4,
+                    dotColor:
+                        AppColors.of(context).consistentPrimary.lighten(0.8),
+                    activeDotColor: AppColors.of(context).consistentPrimary,
+                    spacing: spacing.toDouble(),
+                    dotWidth: constraints.maxWidth /
+                            (items.length + expansionFactor - 1) -
+                        spacing / items.length,
+                    expansionFactor: 4,
+                  ),
+                );
+              }),
+            ),
+            Expanded(
+              child: PageView.builder(
+                controller: pageController,
+                itemBuilder: (context, index) {
+                  final item = items.elementAt(index);
+
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: Container(
+                            width: double.infinity,
+                            padding: const EdgeInsets.all(16),
+                            margin: const EdgeInsets.only(
+                                left: 8, right: 8, top: 16, bottom: 0),
+                            decoration: BoxDecoration(
+                              color: isAppInDarkBrightness(context)
+                                  ? Theme.of(context)
+                                      .colorScheme
+                                      .primary
+                                      .lighten(0.8)
+                                  : Theme.of(context)
+                                      .colorScheme
+                                      .primaryContainer
+                                      .lighten(0.58),
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            child: SvgPicture.asset(
+                              item['image'],
+                              fit: BoxFit.contain,
+                              width: 240.0,
+                              alignment: Alignment.bottomCenter,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 24),
+                        Expanded(
+                          flex: 1,
+                          child: SingleChildScrollView(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  item['header'],
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .titleLarge!
+                                      .copyWith(fontWeight: FontWeight.bold),
+                                  //  textAlign: TextAlign.center,
+                                ),
+                                const SizedBox(height: 6),
+                                Text(item['description']),
+                                if (item['description2'] != null) ...[
+                                  const SizedBox(height: 6),
+                                  Text(item['description2']),
+                                ],
+                                if (item['extraWidget'] != null)
+                                  item['extraWidget'],
+                                const SizedBox(height: 64),
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                },
+                itemCount: items.length,
+                onPageChanged: (pageIndex) {
+                  setState(() {
+                    currentPage = pageIndex;
+                  });
+                },
+                physics: const BouncingScrollPhysics(),
+              ),
+            ),
+          ],
         ));
+  }
+
+  StreamBuilder<Currency> buildCurrencySelectorTile(BuildContext context) {
+    final t = Translations.of(context);
+
+    return StreamBuilder(
+      stream: CurrencyService.instance.getUserPreferredCurrency(),
+      builder: (context, snapshot) {
+        final userCurrency = snapshot.data;
+
+        return ListTile(
+          tileColor: Theme.of(context).colorScheme.onSurface.withOpacity(0.04),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          trailing: Icon(
+            Icons.arrow_forward_ios_rounded,
+            size: 14,
+            color: Theme.of(context).colorScheme.onSurface.withOpacity(0.45),
+          ),
+          leading: Container(
+            clipBehavior: Clip.hardEdge,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(100),
+            ),
+            child: userCurrency != null
+                ? userCurrency.displayFlagIcon(size: 42)
+                : const Skeleton(height: 42, width: 42),
+          ),
+          title: Text(t.intro.select_your_currency),
+          subtitle: userCurrency != null
+              ? Text(userCurrency.name)
+              : const Skeleton(height: 12, width: 50),
+          onTap: () {
+            if (userCurrency == null) return;
+
+            showCurrencySelectorModal(
+              context,
+              CurrencySelectorModal(
+                  preselectedCurrency: userCurrency,
+                  onCurrencySelected: (newCurrency) {
+                    UserSettingService.instance
+                        .setItem(SettingKey.preferredCurrency, newCurrency.code)
+                        .then((value) => setState(() => {}));
+                  }),
+            );
+          },
+        );
+      },
+    );
   }
 }
