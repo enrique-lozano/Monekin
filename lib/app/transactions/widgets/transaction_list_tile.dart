88ac3b104182e04e35e528bba3b2486e7df37452
Zafcar <Devishcomp2012@gmail.com>
2025-07-22 19:16:39 +0530

    Added delete and reconciled functionality on swipe

--- lib/app/transactions/widgets/transaction_list_tile.dart
+++ lib/app/transactions/widgets/transaction_list_tile.dart
@@ -45,7 +45,7 @@ class TransactionListTile extends StatelessWidget {
   final void Function()? onTap;
 
   final bool isSelected;
-
+  
   showTransactionActions(BuildContext context, MoneyTransaction transaction) {
     showModalBottomSheet(
         context: context,
@@ -72,196 +72,225 @@ class TransactionListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Row(
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Expanded(
-            child: Row(
-              children: [
-                Flexible(
-                  child: Text(
-                    transaction.displayName(context),
-                    softWrap: false,
-                    overflow: TextOverflow.fade,
-                  ),
-                ),
-                if ((transaction.status != null ||
-                        transaction.recurrentInfo.isRecurrent) &&
-                    periodicityInfo == null) ...[
-                  const SizedBox(width: 4),
-                  Icon(
-                    transaction.status?.icon ?? Icons.repeat,
-                    color: transaction.status?.color.darken(0.1) ??
-                        Theme.of(context).colorScheme.primary,
-                    size: 12,
-                  )
-                ],
-                if (transaction.isReversed) ...[
-                  const SizedBox(width: 6),
-                  Icon(
-                    MoneyTransaction.reversedIcon,
-                    size: 12,
-                    color: AppColors.of(context).brand,
-                  )
-                ]
-              ],
-            ),
-          ),
-          CurrencyDisplayer(
-            amountToConvert: periodicityInfo != null
-                ? transaction.getUnifiedMoneyForAPeriod(
-                    periodicity: periodicityInfo!)
-                : transaction.value,
-            currency: transaction.account.currency,
-            integerStyle: TextStyle(
-                color: transaction.status == TransactionStatus.voided
-                    ? Colors.grey.shade400
-                    : transaction.isIncomeOrExpense
-                        ? transaction.type.color(context)
-                        : null,
-                decoration: transaction.status == TransactionStatus.voided
-                    ? TextDecoration.lineThrough
-                    : null,
-                fontWeight: FontWeight.bold),
-          ),
-        ],
-      ),
-      subtitle: DefaultTextStyle(
-        style: Theme.of(context)
-            .textTheme
-            .labelSmall!
-            .copyWith(fontWeight: FontWeight.w300),
+    return Dismissible(
+      key: Key(transaction.id),
+      background: Container(
+        color: Colors.green,
         child: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Expanded(
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Flexible(
-                    child: Builder(builder: (context) {
-                      String secondaryText = '';
-
-                      if (!(transaction.recurrentInfo.isRecurrent &&
-                          periodicityInfo != null)) {
-                        if (showDate) {
-                          secondaryText =
-                              DateFormat.yMMMd().format(transaction.date);
-                        } else {
-                          secondaryText = transaction.notes ?? '';
-                        }
-                      }
-
-                      return Text(
-                        '${transaction.account.name} ${secondaryText.isNotEmpty ? ('• $secondaryText') : ''}',
-                        softWrap: false,
-                        overflow: TextOverflow.fade,
-                      );
-                    }),
-                  ),
-                  if (transaction.recurrentInfo.isRecurrent &&
-                      periodicityInfo != null) ...[
-                    const SizedBox(width: 4),
-                    Container(
-                      padding: const EdgeInsets.fromLTRB(6, 0, 6, 0),
-                      decoration: BoxDecoration(
-                        color: transaction.nextPayStatus!
-                            .color(context)
-                            .withOpacity(0.2),
-                        border: Border.all(
-                          color: transaction.nextPayStatus!.color(context),
-                          width: 0.2,
-                        ),
-                        borderRadius: BorderRadius.circular(4),
-                      ),
-                      child: Row(
-                        children: [
-                          Icon(
-                            transaction.nextPayStatus!.icon,
-                            size: 14,
-                            color: transaction.nextPayStatus!.color(context),
-                          ),
-                          const SizedBox(width: 4),
-                          Text(
-                              transaction.nextPayStatus!.displayDaysToPay(
-                                  context, transaction.daysToPay()),
-                              style: TextStyle(
-                                fontSize: 12,
-                                fontWeight: FontWeight.w400,
-                                color:
-                                    transaction.nextPayStatus!.color(context),
-                              ))
-                        ],
-                      ),
-                    ),
-                    const SizedBox(height: 2),
-                  ]
-                ],
-              ),
-            ),
-            if (showTime) Text(DateFormat.Hm().format(transaction.date)),
-            if (periodicityInfo != null &&
-                transaction.recurrentInfo.isRecurrent)
-              Text.rich(
-                TextSpan(children: [
-                  ...UINumberFormatter.currency(
-                          amountToConvert: transaction.value,
-                          currency: transaction.account.currency)
-                      .getTextSpanList(context),
-                  if (transaction.recurrentInfo.intervalEach! != 1)
-                    TextSpan(
-                        text:
-                            ' / ${transaction.recurrentInfo.intervalEach!.toStringAsFixed(0)}'),
-                  if (transaction.recurrentInfo.intervalEach! == 1)
-                    const TextSpan(text: ' / '),
-                  TextSpan(
-                      text: transaction.recurrentInfo.intervalPeriod!
-                          .periodText(
-                            context,
-                            isPlural:
-                                transaction.recurrentInfo.intervalEach! > 1,
-                          )
-                          .toLowerCase())
-                ]),
-              )
+            Text('Accepted')
           ],
         ),
       ),
-      leading: Hero(
-        tag: heroTag ?? UniqueKey(),
-        child: isSelected
-            ? Stack(
-                alignment: Alignment.center,
-                children: [
-                  const Icon(Icons.circle, size: 28 + 12),
-                  Icon(
-                    Icons.check,
-                    size: 24,
-                    color: Theme.of(context).colorScheme.surface,
-                  ),
-                ],
-              )
-            : transaction.getDisplayIcon(context, size: 28, padding: 6),
+      secondaryBackground: Container(
+        color: Colors.red,
+        child: Row(
+          children: [
+            Text('Delete')
+          ],
+        ),
       ),
-      selected: isSelected,
-      selectedTileColor:
-          Theme.of(context).colorScheme.primary.withOpacity(0.15),
-      onTap: onTap ??
-          () {
-            RouteUtils.pushRoute(
-              context,
-              TransactionDetailsPage(
-                transaction: transaction,
-                heroTag: heroTag,
-                prevPage: prevPage,
-              ),
-            );
+      confirmDismiss: (direction) async {
+            if (direction == DismissDirection.startToEnd) {
+              debugPrint("Swipe Right: Action 1 (e.g., Mark as Done)");
+              await TransactionViewActionService().updateTransactionStatus(transaction.id, "reconcidel");
+            } else if (direction == DismissDirection.endToStart) {
+              debugPrint("Swipe Left: Action 2 (e.g., Show Info)");
+              TransactionViewActionService().deleteTransactionWithAlertAndSnackBar(context, transactionId: transaction.id, navigateBack: false);
+            }
+            return false;
           },
-      onLongPress:
-          onLongPress ?? () => showTransactionActions(context, transaction),
+      child: ListTile(
+            title: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Row(
+                    children: [
+                      Flexible(
+                        child: Text(
+                          transaction.displayName(context),
+                          softWrap: false,
+                          overflow: TextOverflow.fade,
+                        ),
+                      ),
+                      if ((transaction.status != null ||
+                              transaction.recurrentInfo.isRecurrent) &&
+                          periodicityInfo == null) ...[
+                        const SizedBox(width: 4),
+                        Icon(
+                          transaction.status?.icon ?? Icons.repeat,
+                          color: transaction.status?.color.darken(0.1) ??
+                              Theme.of(context).colorScheme.primary,
+                          size: 12,
+                        )
+                      ],
+                      if (transaction.isReversed) ...[
+                        const SizedBox(width: 6),
+                        Icon(
+                          MoneyTransaction.reversedIcon,
+                          size: 12,
+                          color: AppColors.of(context).brand,
+                        )
+                      ]
+                    ],
+                  ),
+                ),
+                CurrencyDisplayer(
+                  amountToConvert: periodicityInfo != null
+                      ? transaction.getUnifiedMoneyForAPeriod(
+                          periodicity: periodicityInfo!)
+                      : transaction.value,
+                  currency: transaction.account.currency,
+                  integerStyle: TextStyle(
+                      color: transaction.status == TransactionStatus.voided
+                          ? Colors.grey.shade400
+                          : transaction.isIncomeOrExpense
+                              ? transaction.type.color(context)
+                              : null,
+                      decoration: transaction.status == TransactionStatus.voided
+                          ? TextDecoration.lineThrough
+                          : null,
+                      fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+            subtitle: DefaultTextStyle(
+              style: Theme.of(context)
+                  .textTheme
+                  .labelSmall!
+                  .copyWith(fontWeight: FontWeight.w300),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Flexible(
+                          child: Builder(builder: (context) {
+                            String secondaryText = '';
+
+                            if (!(transaction.recurrentInfo.isRecurrent &&
+                                periodicityInfo != null)) {
+                              if (showDate) {
+                                secondaryText =
+                                    DateFormat.yMMMd().format(transaction.date);
+                              } else {
+                                secondaryText = transaction.notes ?? '';
+                              }
+                            }
+
+                            return Text(
+                              '${transaction.account.name} ${secondaryText.isNotEmpty ? ('• $secondaryText') : ''}',
+                              softWrap: false,
+                              overflow: TextOverflow.fade,
+                            );
+                          }),
+                        ),
+                        if (transaction.recurrentInfo.isRecurrent &&
+                            periodicityInfo != null) ...[
+                          const SizedBox(width: 4),
+                          Container(
+                            padding: const EdgeInsets.fromLTRB(6, 0, 6, 0),
+                            decoration: BoxDecoration(
+                              color: transaction.nextPayStatus!
+                                  .color(context)
+                                  .withOpacity(0.2),
+                              border: Border.all(
+                                color: transaction.nextPayStatus!.color(context),
+                                width: 0.2,
+                              ),
+                              borderRadius: BorderRadius.circular(4),
+                            ),
+                            child: Row(
+                              children: [
+                                Icon(
+                                  transaction.nextPayStatus!.icon,
+                                  size: 14,
+                                  color: transaction.nextPayStatus!.color(context),
+                                ),
+                                const SizedBox(width: 4),
+                                Text(
+                                    transaction.nextPayStatus!.displayDaysToPay(
+                                        context, transaction.daysToPay()),
+                                    style: TextStyle(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.w400,
+                                      color:
+                                          transaction.nextPayStatus!.color(context),
+                                    ))
+                              ],
+                            ),
+                          ),
+                          const SizedBox(height: 2),
+                        ]
+                      ],
+                    ),
+                  ),
+                  if (showTime) Text(DateFormat.Hm().format(transaction.date)),
+                  if (periodicityInfo != null &&
+                      transaction.recurrentInfo.isRecurrent)
+                    Text.rich(
+                      TextSpan(children: [
+                        ...UINumberFormatter.currency(
+                                amountToConvert: transaction.value,
+                                currency: transaction.account.currency)
+                            .getTextSpanList(context),
+                        if (transaction.recurrentInfo.intervalEach! != 1)
+                          TextSpan(
+                              text:
+                                  ' / ${transaction.recurrentInfo.intervalEach!.toStringAsFixed(0)}'),
+                        if (transaction.recurrentInfo.intervalEach! == 1)
+                          const TextSpan(text: ' / '),
+                        TextSpan(
+                            text: transaction.recurrentInfo.intervalPeriod!
+                                .periodText(
+                                  context,
+                                  isPlural:
+                                      transaction.recurrentInfo.intervalEach! > 1,
+                                )
+                                .toLowerCase())
+                      ]),
+                    )
+                ],
+              ),
+            ),
+            leading: Hero(
+              tag: heroTag ?? UniqueKey(),
+              child: isSelected
+                  ? Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        const Icon(Icons.circle, size: 28 + 12),
+                        Icon(
+                          Icons.check,
+                          size: 24,
+                          color: Theme.of(context).colorScheme.surface,
+                        ),
+                      ],
+                    )
+                  : transaction.getDisplayIcon(context, size: 28, padding: 6),
+            ),
+            selected: isSelected,
+            selectedTileColor:
+                Theme.of(context).colorScheme.primary.withOpacity(0.15),
+            onTap: onTap ??
+                () {
+                  RouteUtils.pushRoute(
+                    context,
+                    TransactionDetailsPage(
+                      transaction: transaction,
+                      heroTag: heroTag,
+                      prevPage: prevPage,
+                    ),
+                  );
+                },
+            onLongPress:
+                onLongPress ?? () => showTransactionActions(context, transaction),
+          )
     );
   }
 }
--- lib/core/services/view-actions/transaction_view_actions_service.dart
+++ lib/core/services/view-actions/transaction_view_actions_service.dart
@@ -1,8 +1,11 @@
+import 'package:drift/drift.dart';
+import 'package:drift/src/runtime/query_builder/query_builder.dart';
 import 'package:flutter/material.dart';
 import 'package:monekin/app/transactions/form/transaction_form.page.dart';
 import 'package:monekin/core/database/app_db.dart';
 import 'package:monekin/core/database/services/transaction/transaction_service.dart';
 import 'package:monekin/core/models/transaction/transaction.dart';
+import 'package:monekin/core/models/transaction/transaction_status.enum.dart';
 import 'package:monekin/core/presentation/widgets/confirm_dialog.dart';
 import 'package:monekin/core/routes/route_utils.dart';
 import 'package:monekin/core/utils/list_tile_action_item.dart';
@@ -143,5 +146,13 @@ class TransactionViewActionService {
             TransactionTag(transactionID: newTrId, tagID: tag.id),
           );
     }
+  }
+
+  Future<int> updateTransactionStatus(String transactionId, String newStatusCode) {
+    final db = AppDB.instance;
+
+    return(db.update(db.transactions)
+      ..where((transaction) => transaction.id.equals(transactionId)))
+      .write(TransactionsCompanion(status:  Value(TransactionStatus.reconciled)));
   }
 }
