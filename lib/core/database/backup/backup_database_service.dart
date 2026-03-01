9e1f79c8ec0115062979e7e6ecf7136085227fb1
Diego Miguel Lozano <hello@diegomiguel.me>
2026-03-01 16:24:54 +0100

    feat(ux/ui): Include tags in CSV exports

--- lib/core/database/backup/backup_database_service.dart
+++ lib/core/database/backup/backup_database_service.dart
@@ -44,48 +44,51 @@ class BackupDatabaseService {
   Future<Uint8List> getDbFileInBytes() async =>
       File(await db.databasePath).readAsBytes();
 
-  String creatCsvFromTransactions(
+  String createCsvFromTransactions(
     List<MoneyTransaction> data, {
     String format = 'csv',
-    String separator = ',',
+    String fieldSeparator = ',',
+    String listSeparator = ';',
   }) {
-    var csvData = '';
-
-    var keys = [
-      'ID',
-      'Amount',
-      'Date',
-      'Title',
-      'Note',
-      'Account',
-      'Currency',
-      'Category',
-      'Subcategory',
-    ];
-
-    if (data.isNotEmpty) {
-      for (final key in keys) {
-        csvData += key + separator;
-      }
-    }
-
-    csvData += '\n';
-
     final dateFormatter = DateFormat('yyyy-MM-dd HH:mm:ss');
+    final processedData = <List<String?>>[
+      // Header
+      [
+        'ID',
+        'Amount',
+        'Date',
+        'Title',
+        'Note',
+        'Account',
+        'Currency',
+        'Category',
+        'Subcategory',
+        'Tags',
+      ],
+    ];
 
     for (final transaction in data) {
       final toAdd = [
+        // ID
         transaction.id,
+        // Amount
         transaction.value.toStringAsFixed(2),
+        // Date
         dateFormatter.format(transaction.date),
+        // Title
         transaction.title ?? '',
+        // Notes
         transaction.notes ?? '',
+        // Account
         transaction.account.name,
+        // Currency
         transaction.account.currencyId,
+        // Category
         if (transaction.isIncomeOrExpense)
           (transaction.category!.parentCategory != null
               ? transaction.category!.parentCategory!.name
               : transaction.category!.name),
+        // Subcategory
         if (transaction.isTransfer) 'TRANSFER',
         (transaction.category?.parentCategory != null
             ? transaction.category?.name
@@ -93,18 +96,13 @@ class BackupDatabaseService {
         // Tags
         transaction.tags.map((e) => e.name).join(listSeparator),
       ];
-
-      csvData += toAdd.join(separator);
-
-      csvData += '\n';
+      processedData.add(toAdd);
 
       if (transaction.isTransfer) {
-        csvData += toAdd.join(separator);
-
-        csvData += '\n';
+        processedData.add(toAdd);
       }
     }
-
+    final csvData = ListToCsvConverter().convert(processedData);
     return csvData;
   }
 
@@ -112,12 +110,12 @@ class BackupDatabaseService {
     String exportPath,
     List<MoneyTransaction> data,
   ) async {
-    final csvData = creatCsvFromTransactions(data);
+    final csvData = createCsvFromTransactions(data);
 
     final file = createAndReturnFile(
       exportPath: exportPath,
       fileName:
-          "Transactions-${DateFormat('yyyyMMdd-Hms').format(DateTime.now())}.csv",
+          "Transactions-${DateFormat('yyyyMMdd-HHmmss').format(DateTime.now())}.csv",
     );
 
     return file.writeAsString(csvData, mode: FileMode.writeOnly);
